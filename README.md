ae42c57dee56b7e9e074be551683bccc3d84245e
1andyn <andy.work.dev@gmail.com>
2021-12-15 00:37:20 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# scavenger-webapp-cs
+Simple Virtual Scavenger Hunt implemented as a React Single page Application protecting each step with a password.
+This is the client sided only version which will not have a protected backend to prevent source code peeking from seeing passwords. The purpose of this is to be able to host this on a free platform such as github or gitlab as a page repo branch.
+
+**Planned functionality:**
+ - Display an Introduction
+ - Shows initial scavenger hunt step (image and clues and a password prompt)
+ - Entering the correct password will transition into the next step
+
+# dependencies
+ - react for front end
+	 - react-spring (for animations to look nicer)
+	 - bootstrap (UI)
+ - dotenv (for step details and password)
+
+# .dotenv file
+- used to store details and passwords, format TBD
---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Col, FormGroup, Input, Row, Spinner } from 'reactstrap';
+import logo from '../logo.svg';
+import image from '../placeholder/image.jpg'
+import '../css/app.css';
+import axios from 'axios';
+
+const App = () => {
+
+  const [initialized, setInitialized] = useState<boolean>(false)
+
+  const [showInterim, setShowInterim] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [next, setNext] = useState<string>('');
+
+  const [imageSource, setImageSource] = useState<string>(image);
+  const [textQueue, setTextQueue] = useState<string>('There is nothing in the queue...');
+  const [textBlock, setTextBlock] = useState<string>('Welcome to the Scavenger, click continue to Start!');
+
+  /* Run Once Initialization */
+  useEffect(() => {
+    if(!initialized) {
+      setInitialized(true)
+      console.log('The hook has run!')
+    }
+  }, [initialized]);
+
+  const clickContinue = () => {
+    setTextBlock(textQueue); //Set text to be the next prompt
+    setShowInterim(false); //Hide interim logic
+
+  }
+
+
+  return (
+    <div className="d-flex align-items-center min-vh-100">
+      <div className="container">
+        <Row>
+          <Col lg={4} className='m-4' id='imageBox'>
+            <div className='d-flex justify-content-center'><img className='p-4 imageBlock' src={imageSource} /></div>
+          </Col>
+          <Col lg={4} className='m-4' id='textBox'>
+              <h1 className='p-4 align-middle'>{textBlock}</h1>
+          </Col>
+        </Row>
+        {
+          isLoading ? (<div className='d-flex justify-content-center'><Spinner>Loading...</Spinner></div>) :
+            showInterim ? (
+              <Row>
+                <Col lg={5}/>
+                <Col lg={2}><Button onClick={ ()=> clickContinue()}>Continue</Button></Col>
+                <Col lg={5}/>
+              </Row>
+            ) : (
+              <Row>
+                <div className='d-flex justify-content-center'>
+                  <FormGroup>
+                    <Input
+                      id="inputPassword"
+                      name="password"
+                      placeholder="answer"
+                      type="password"
+                    />
+                    <Button>Submit</Button>
+                  </FormGroup>
+                </div>
+              </Row>
+            )
+        }
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
