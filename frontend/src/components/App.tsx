f4cf3d2f44d9c535af356a1ea75d9831858e0c11
1andyn <andy.work.dev@gmail.com>
2021-12-16 03:24:43 -0800

    finished basic functionality hunt

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -15,43 +15,84 @@ const App = () => {
 
   const [initialized, setInitialized] = useState<boolean>(false)
 
-  const [showInterim, setShowInterim] = useState<boolean>(true);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [stepData, setStepData] = useState<any[]>()
-  const [next, setNext] = useState<string[]>();
-  const [stepCount, setStepCounter] = useState<number>(0)
+  const [showInterim, setShowInterim] = useState<boolean>(true)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [stepCount, setStepCount] = useState<number>(0)
+  const [hideButtons, setHidebuttons] = useState<boolean>(false)
 
   let testImages = [
     image,
     image2,
     image3
   ]
+  
+  const [stepData, setStepData] = useState<{
+    prompt: string, 
+    images: string[],
+    answer: string,
+    intermission: string}[]>([])
 
   const [imageSource, setImageSource] = useState<string[]>(testImages);
+  const [imageQueue, setImageQueue] = useState<string[]>([]);
   const [textQueue, setTextQueue] = useState<string>('There is nothing in the queue...');
   const [textBlock, setTextBlock] = useState<string>('Welcome to the Scavenger Hunt, click continue to Start!');
+
+  const [answer, setAnswer] = useState<string>()
 
   /* Run Once Initialization */
   useEffect(() => {
     if(!initialized) {
       setInitialized(true)
-      var data = Loader(text)
-      setStepData(data)
-      setIsLoading(false)
-      console.log('The hook has run!')
+      Loader(text).then( data => {
+        setStepData(data)
+      })
     }
   }, [initialized]);
 
-  const clickContinue = () => {
+  useEffect(() => {
+    if(stepData.length !== 0) {
+      initialize()
+      setIsLoading(false)
+    }
+  }, [stepData]);
+
+  const timeout = (delay: number) => {
+    return new Promise( res => setTimeout(res, delay) );
+  }
+
+  const initialize = () => {
+    if(typeof stepData != 'undefined') {
+      setTextQueue(stepData[stepCount].prompt)
+      setImageQueue(stepData[stepCount].images)
+    }
+  }
+
+  const clickContinue = async () => {
+    setIsLoading(true)
+    await timeout(500)
+    setIsLoading(false)
     setTextBlock(textQueue); //Set text to be the next prompt
+    setImageSource(imageQueue);
     setShowInterim(false); //Hide interim logic
-
+    setTextQueue(stepData[stepCount].intermission)
   }
 
-  const checkAnswer = (text : string) => {
-
+  const checkAnswer = () => {
+    if (answer === stepData[stepCount].answer) {
+      setTextBlock(textQueue) //this should be showing intermission
+      if(stepCount+1 == stepData.length) {
+        setHidebuttons(true)
+      } else {
+        setTextQueue(stepData[stepCount+1].prompt)
+        setImageQueue(stepData[stepCount+1].images)
+        setStepCount(stepCount+1)
+        setAnswer('')
+        setShowInterim(true)
+      }
+    } else {
+      alert("You guessed wrong nerd!")
+    }
   }
-
 
   return (
     <div className="d-flex align-items-center min-vh-100">
@@ -64,7 +105,7 @@ const App = () => {
               { TextBlock(textBlock) }
           </Col>
         </Row>
-        {
+        { hideButtons ? null :
           isLoading ? (<div className='d-flex justify-content-center'><Spinner>Loading...</Spinner></div>) :
             showInterim ? (
               <Row>
@@ -80,8 +121,10 @@ const App = () => {
                       name="password"
                       placeholder="answer"
                       type="password"
+                      value={answer}
+                      onChange={ (e) => setAnswer(e.target.value)}
                     />
-                    <Button>{'>'}</Button></InputGroup>
+                    <Button onClick={ () => checkAnswer() }>{'>'}</Button></InputGroup>
                   </FormGroup>
                 </div>
               </Row>
